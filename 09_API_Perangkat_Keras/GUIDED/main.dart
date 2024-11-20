2ceceae318ac7b883b88c866c4e3dadb97e47cd5
Irfan Muria <137787553+irpanzy@users.noreply.github.com>
2024-11-20 11:50:57 +0700

    Update main.dart

--- 09_API_Perangkat_Keras/GUIDED/main.dart
+++ 09_API_Perangkat_Keras/GUIDED/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:guided/camera_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyCameraScreen(),
+    );
+  }
+}
